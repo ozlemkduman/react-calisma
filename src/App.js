4f2daad89a67a5281529bdb3747c305ae2975215
ozlemkduman <ozlemk.duman@gmail.com>
2023-04-26 17:59:14 +0300

    add App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Header from './components/Header.js'
 import Main from './components/Main.js'
 
-let name="Özlem"
-let lastName="Karaduman"
+let name="Özlem";
+let lastName="Karaduman";
+let isActive=true;
+let greating="Hoşgeldiniz"
 function App() {
   return (
     <div className="App">
       <Header />
       <p>{`Burası ${name} ${lastName}'ın çalışma alanıdır.`}</p>
+      <p>{isActive ? greating : "Üye değilsiniz"}</p>
       <Main/>
     </div>
   );
